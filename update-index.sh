2223c984b4e252eb96013e8093f0cf49407d5767
Samsul Ma'arif <samsul@samsul.web.id>
2014-09-17 19:14:32 +0700

    Update enable for amd64 repos
     * As well as for i386
     * Now using `for` :-)
    
    Signed-off-by: Samsul Ma'arif <samsul@samsul.web.id>

--- update-index.sh
+++ update-index.sh
@@ -3,14 +3,24 @@
 # i bought that repos from an online shop, and make it 
 # as an offline repo for my workstation.
 
-# first create the target folder
-mkdir -p dists/trusty/{main,extras,multiverse,restricted,universe}/binary-i386;
+# either i386 or amd64
+arch=amd64
+
+# distribution codename
+code=trusty
 
 # create the Packages.gz for every component
 # this file is an index for every packages in every component
-dpkg-scanpackages pool/main /dev/null | gzip -9c > dists/trusty/main/binary-i386/Packages.gz ;
-dpkg-scanpackages pool/extras /dev/null | gzip -9c > dists/trusty/extras/binary-i386/Packages.gz ;
-dpkg-scanpackages pool/multiverse /dev/null | gzip -9c > dists/trusty/multiverse/binary-i386/Packages.gz ;
-dpkg-scanpackages pool/restricted /dev/null | gzip -9c > dists/trusty/restricted/binary-i386/Packages.gz ;
-dpkg-scanpackages pool/universe /dev/null | gzip -9c > dists/trusty/universe/binary-i386/Packages.gz ;
+for c in {main,extras,multiverse,restricted,universe};
+do
+	# first create the target folder
+	echo "Creating directory dists/$code/$c/binary-$arch"; 
+	mkdir -p dists/$code/$c/binary-$arch;
+
+	# create package index
+	echo "Creating package index for $c in dists/$code/$c/binary-$arch/Packages.gz";
+	dpkg-scanpackages pool/$c /dev/null | gzip -9c > dists/$code/$c/binary-$arch/Packages.gz ;
+	sleep 3
+	echo "All DONE!";
+done
 exit 0
